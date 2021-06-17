f6f17a678872e0ad6a5b4fe3ade7346d4ea2fcf3
LeandroMoura124 <leofcb334@gmail.com>
2021-06-17 11:27:36 -0300

    Adicionar arquivos de projeto.

---
+++ Projetovalidar/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projetovalidar.Models;
+
+namespace Projetovalidar.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            var usuario = new Usuario(); //criei obejto 
+            return View(usuario); //retornei para view os dados
+        }
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid) // validando o estado , ou seja, verificando a requisicao
+            {
+                return View("Resultado", usuario);
+            }
+            return View(usuario);
+        }
+        public ActionResult Resultado(Usuario usuario)
+        {
+            return View(usuario);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bdExemplo = new Collection<string>
+            {
+
+                "Leandro",
+                "TravisScott"
+            };
+            return Json(bdExemplo.All(x => x.ToLower() != login.ToLower()),
+                                        JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Projetovalidar/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+
+namespace Projetovalidar.Models
+{
+	public class Usuario
+	{
+		[Required(ErrorMessage = "O nome é obrigatório")]
+		public string Nome { get; set; }
+
+		[StringLength(50, MinimumLength = 5, ErrorMessage = "Insira de 5 a 50 caracteres.")]
+		public string Observacao { get; set; }
+
+		[Range(18, 70, ErrorMessage = "Idade permitida entre 18 e 70!")]
+		public int Idade { get; set; }
+
+		[RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email inválido.")]
+		public string Email { get; set; }
+
+		[RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "Somente letras de 5 a 15 caracteres")]
+		[Required(ErrorMessage = "Login obrigatório")]
+		[Remote("LoginUnico", "usuario", ErrorMessage = "Login já cadastrado")]
+		public string Login { get; set; }
+
+		[Required(ErrorMessage = "Senha obrigatório")]
+		public string Senha { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "A senha não corresponde.")]
+		public string Confsenha { get; set; }
+
+
+	}
+}
